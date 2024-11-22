8218a78f0442cc8b3549385798cdd15ffb48eab5
Pete399c1 <cph-pm260@cphbusiness.dk>
2024-11-22 11:40:22 +0100

    Classes with attributes series

--- src/Series.java
+++ src/Series.java
@@ -2,5 +2,4 @@ public class Series extends Media {
     public Series(String mediaTitle,double rating,String genre,String releaseYear, String seasonsAndEpisodes){
         super(mediaTitle,rating,genre,releaseYear,seasonsAndEpisodes);
     }
-
 }
