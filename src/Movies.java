06a228024bab425728dc07abf2cb9610f5754f44
Pete399c1 <cph-pm260@cphbusiness.dk>
2024-11-22 11:29:33 +0100

    Class with attributes movies

--- src/Movies.java
+++ src/Movies.java
@@ -3,5 +3,4 @@ public class Movies extends Media {
     public Movies(String mediaTitle,double rating,String genre,String releaseYear,String seasonsAndEpisodes){
         super(mediaTitle,rating,genre,releaseYear);
     }
-
 }
