663621c0bc9ac340f62338b8f8de1f8dc808133d
EmilDagsberg <cph-ed113@cphbusiness.dk>
2024-11-25 10:25:12 +0100

    watchlist

--- src/User.java
+++ src/User.java
@@ -8,10 +8,27 @@ public class User {
     User(String username, String password) {
         this.username = username;
         this.password = password;
+        this.watchlist = new ArrayList<>();
     }
 
     @Override
     public String toString() {
         return this.username + ", " + this.password;
+    }
+
+
+    public void addToWatchlist(Media media) {
+        watchlist.add(media);
+    }
+
+    public void displayWatchlist() {
+        if(watchlist.isEmpty()) {
+            ui.displayMsg("Your watchlist is empty");
+        } else {
+            ui.displayMsg("Your watchlist:");
+            for(Media media : watchlist) {
+                ui.displayMsg(media.getMediaTitle());
+            }
+        }
     }
 }
