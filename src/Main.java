eda982de58fdbfa47a342dedf7b98b3bc1d7b965
Emil Kriegel <54576311+emil2112@users.noreply.github.com>
2024-11-28 11:08:44 +0100

    Afleveringsklar 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        StreamingService s = new StreamingService("peepeepoopoo");
+        StreamingService s = new StreamingService("Meepflix");
         s.startStreamingService();
     }
 }
