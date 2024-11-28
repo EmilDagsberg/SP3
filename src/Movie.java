4b19a5c9c3ec270611bd6c02c93f80834d0ce891
EmilDagsberg <cph-ed113@cphbusiness.dk>
2024-11-28 09:40:06 +0100

    Ændringer

--- src/Movie.java
+++ src/Movie.java
@@ -12,7 +12,8 @@ public class Movie extends Media {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) { // Denne method sammenligner 2 objekter og kigger på deres 3 fields(mediaTitle, releaseYear, genre):
+        // Hvis det er ens, så er objekterne set som det samme. Ellers er de ikke.
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Movie movie = (Movie) o;
@@ -21,8 +22,10 @@ public class Movie extends Media {
                 genre.equals(movie.genre);
     }
 
+
     @Override
-    public int hashCode() {
+    public int hashCode() { // Laver en hashcode(integer value tildelt hvert objekt).
+        // Hvis 2 objekter er equal(returns true), så skal deres hashcode også være det samme. Det er dette metoden gør.
         return Objects.hash(mediaTitle, releaseYear, genre);
     }
 
