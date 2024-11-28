86acb934797614dc57e611b5b559629809c5b35b
EmilDagsberg <cph-ed113@cphbusiness.dk>
2024-11-28 09:40:56 +0100

    Ændringer

--- src/Series.java
+++ src/Series.java
@@ -15,7 +15,8 @@ public class Series extends Media {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) { // Denne method sammenligner 2 objekter og kigger på deres 3 fields(mediaTitle, releaseYear, genre):
+        // Hvis det er ens, så er objekterne set som det samme. Ellers er de ikke.
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Series series = (Series) o;
@@ -25,7 +26,8 @@ public class Series extends Media {
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode() {// Laver en hashcode(integer value tildelt hvert objekt).
+        // Hvis 2 objekter er equal(returns true), så skal deres hashcode også være det samme. Det er dette metoden gør.
         return Objects.hash(mediaTitle, releaseYear, genre);
     }
 }
